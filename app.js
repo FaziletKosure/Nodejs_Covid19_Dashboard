4a9874338e4f01b9675a47b2917bbaa3fa1585dc
FaziletKosure <faziletkosure1@gmail.com>
2021-01-29 21:40:15 +0100

    add

--- app.js
+++ app.js
@@ -10,10 +10,16 @@ app.set('views','./views');
 app.set('view engine','ejs');
 app.use(express.static('./public'));
 
-app.get('/*',async(req,res)=>{
+app.get('/',async(req,res)=>{
     const global= await api.all();
     const countries= await api.countries({sort:'cases'})
     res.render('index',{global,countries});
+})
+
+app.get('/yesterday',async(req,res)=>{
+    const yesglobal= await api.yesterday.all();
+    const yescountries= await api.yesterday.countries({sort:'cases'})
+    res.render('yesterday',{yesglobal,yescountries});
 })
 
 app.listen(port,()=>console.log('running on port 8080'))
