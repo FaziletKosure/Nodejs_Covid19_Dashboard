d241f818ac154adbc4a0891f35b17ce54ee37639
FaziletKosure <faziletkosure1@gmail.com>
2021-01-31 01:34:03 +0100

    add

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express=require("express")
 const api=require('novelcovid')
+const bodyParser = require('body-parser');
+
+
+var LocalStorage = require('node-localstorage').LocalStorage,
+localStorage = new LocalStorage('./scratch');
 
 const app=express()
 
 const port = process.env.PORT || 8080
 
-
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.set('views','./views');
 app.set('view engine','ejs');
 app.use(express.static('./public'));
@@ -21,14 +26,28 @@ app.get('/yesterday',async(req,res)=>{
     const yescountries= await api.yesterday.countries({sort:'cases'})
     res.render('yesterday',{yesglobal,yescountries});
 })
+app.post('/chart', (req, res) => {
+//    return res.send(`Full name is:${req.body.country}.`);
+ var cntry=(req.body.country).toLowerCase()
+   console.log(cntry)
+   res.redirect('/chart')
+   localStorage.setItem('myFirstKey', cntry)
+  
+  });
 
 app.get('/chart',async(req,res)=>{
-    const mobilityData= await api.historical.countries({country:'belgium',days:7})
-    const dates=Object.keys(mobilityData.timeline.cases)
-    const daethsDates=Object.keys(mobilityData.timeline.deaths)
-    const data=Object.values(mobilityData.timeline.cases)
-    const deathsData=Object.values(mobilityData.timeline.deaths)
+    
+const x=localStorage.getItem('myFirstKey')
+   
+const mobilityData= await api.historical.countries({country:'belgium',days:7})
+const locData= await api.historical.countries({country:x,days:7})
+const dates=Object.keys(x ? locData.timeline.cases : mobilityData.timeline.cases)
+const daethsDates=Object.keys(x ? locData.timeline.deaths : mobilityData.timeline.deaths)
+const data=Object.values(x ? locData.timeline.cases : mobilityData.timeline.cases)
+const deathsData=Object.values(x ? locData.timeline.deaths : mobilityData.timeline.deaths)
     console.log(deathsData)
+    console.log(localStorage.getItem('myFirstKey'))
+    // localStorage.clear();
     // const yescountries= await api.yesterday.countries({sort:'cases'}).then(concole.log)
     res.render('chart',{dates,data,daethsDates,deathsData});
 })
